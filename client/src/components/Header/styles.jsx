db6f04df5467b1ff06922be00aeab36a15db961b
Aniket Agarwal <aniketagarwal57@gmail.com>
2021-10-02 13:40:48 +0530

    Create styles.jsx

---
+++ client/src/components/Header/styles.jsx
@@ -0,0 +1,34 @@
+const styles = {
+  headerContainer: {
+    marginTop: "2rem",
+    marginBottom: "2rem",
+    fontFamily: "Montserrat, Sans-serif",
+  },
+
+  logo: {
+    borderRadius: "50px",
+    width: "6rem",
+    height: "auto",
+    cursor: "pointer",
+  },
+
+  imgContainer: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  navbar: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  navlinks: {
+    marginTop: "1.7rem",
+    marginLeft: "2rem",
+    marginRight: "2rem",
+  },
+};
+
+export default styles;
