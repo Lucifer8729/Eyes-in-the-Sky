43a7e286f7630f602404fbeece00a5e44bcec239
Aniket Agarwal <aniketagarwal57@gmail.com>
2021-10-03 01:55:27 +0530

    changed home

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,11 +2,19 @@ import React from "react";
 import classes from "./styles.module.css";
 
 const Home = () => {
-  return <div>
-    <div className={classes.header}><h1>Lorem Ipsum</h1></div>
-    <div className={classes.body}><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-      eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></div>
-  </div>
+  return (
+    <div>
+      <div className={classes.header}>
+        <h1>SkyLight</h1>
+      </div>
+      <div className={classes.body}>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
