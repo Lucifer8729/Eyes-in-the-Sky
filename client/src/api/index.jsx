c824515b37f79e11848fc4835431cd5f638d8e47
Saurav Kumar <72292326+Lucifer8729@users.noreply.github.com>
2021-10-03 12:32:19 +0530

    .

--- client/src/api/index.jsx
+++ client/src/api/index.jsx
@@ -1,40 +1,46 @@
 import axios from "axios";
+import { dataURLtoFile } from "./utils";
 
-const URL_SERVER = "http://localhost:3004";
+const URL_SERVER = "http://127.0.0.1:8000";
 
 export const postImage = async (data) => {
-    try {
-        const response = await axios({
-            method: "POST",
-            url: `${URL_SERVER}/toProcess`,
-            data: { toProcess: data },
-        });
+  const image = dataURLtoFile(data, "image.jpg");
+  console.log(image);
 
-        // await axios({
-        //   method: "POST",
-        //   url: `${URL_SERVER}/processed`,
-        //   data: { processed: "" },
-        // });
+  try {
+    // const response = await axios({
+    //   method: "POST",
+    //   url: `${URL_SERVER}/predict`,
+    //   data: { detail: image },
+    // });
 
-        return {
-            imageToProcess: response.data.toProcess,
-        };
-    } catch (error) {
-        throw error;
-    }
+    let formData = new FormData();
+    formData.append("file", image);
+    const response = await axios.post(`${URL_SERVER}/predict`, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+
+    return {
+      imageToProcess: response.data.toProcess,
+    };
+  } catch (error) {
+    throw error;
+  }
 };
 
 export const getImage = async () => {
-    try {
-        // const inputImage = await axios.get(`${URL_SERVER}/toProcess`);
-        const response = await axios.get(`${URL_SERVER}/processed`);
+  try {
+    // const inputImage = await axios.get(`${URL_SERVER}/toProcess`);
+    const response = await axios.get(`${URL_SERVER}/processed`);
 
-        // console.log(inputImage);
+    // console.log(inputImage);
 
-        return {
-            processedImage: response.data.processed,
-        };
-    } catch (error) {
-        throw error;
-    }
+    return {
+      processedImage: response.data.processed,
+    };
+  } catch (error) {
+    throw error;
+  }
 };
